3d152ed26735137c478e1ed41c6562548f75a404
Tri Nguyen <tous0905@gmail.com>
2026-04-14 15:37:07 -0400

    Added leafNode

---
+++ src/leafnode.py
@@ -0,0 +1,14 @@
+from htmlnode import HTMLNode
+class LeafNode(HTMLNode):
+  def __init__(self, tag, value, props=None):
+    super().__init__(tag, value, None, props)
+
+  def to_html(self):
+    if not self.value:
+      raise ValueError("value does not exist")
+    if not self.tag:
+      return self.value
+    return f"<{self.tag}>{self.value}</{self.tag}>"
+  
+  def __repr__(self):
+    return f"LeafNode({self.tag}, {self.value}, {self.props})"
