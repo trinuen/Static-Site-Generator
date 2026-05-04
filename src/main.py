836d733828f759d7c7378eb8482eb55a882ad933
Tri Nguyen <tous0905@gmail.com>
2026-05-04 01:06:29 -0400

    Add generate page recursively

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from markdown_to_html import markdown_to_html_node
 from extract_markdown import extract_markdown_images, extract_markdown_links
 def main():
   copy("../static", "../public")
-  generate_page("../content/index.md", "../template.html", "../public/index.html")
+  generate_pages_recursive("../content", "../template.html", "../public/")
 
 def copy(src: str, dest: str):
   if not os.path.exists(src):
@@ -25,25 +25,37 @@ def generate_page(from_path, template_path, dest_path):
   if os.path.isfile(from_path) and os.path.isfile(template_path):
     with open(from_path, "r") as f:
       from_path_content = f.read()
-    # print(from_path_content)
 
     with open(template_path, "r") as f:
       template_path_content = f.read()
-    # print(template_path_content)
 
     content = markdown_to_html_node(from_path_content).to_html()
     title = extract_title(from_path_content)
     template_path_content = template_path_content.replace("{{ Title }}", title)
     template_path_content = template_path_content.replace("{{ Content }}", content)
-    # print(template_path_content)
     directories = extract_markdown_links(from_path_content)
-    # print(directories)
     for directory in directories:
-      # print(directory)
       if directory[1].startswith("/"):
-        # print(f"make {os.path.dirname(dest_path) + directory[1]}")
         os.makedirs(os.path.dirname(dest_path) + directory[1], exist_ok=True)
     with open(dest_path, "w") as f:
       f.write(template_path_content)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+  items_in_content = os.listdir(dir_path_content)
+  
+  for item in items_in_content:
+    full_content_path = os.path.join(dir_path_content, item)
+
+    if os.path.isfile(full_content_path):
+      full_dest_path = os.path.join(dest_dir_path, item)
+      if full_dest_path.endswith(".md"):
+        full_dest_path = full_dest_path.replace("md", "html")
+      print(full_dest_path)
+      generate_page(full_content_path, template_path, full_dest_path)
+  
+    elif os.path.isdir(full_content_path):
+      items_in_subdir = os.listdir(full_content_path)
+      for sub_item in items_in_subdir:
+        items_in_content.append(os.path.join(item, sub_item))
 
 main()
